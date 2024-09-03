0157b9a5205f68ff71d6588642d16d08603ad530
Richard Amodia <richard.t.amodia@gmail.com>
2024-09-03 09:31:25 +0800

    Include whitenoise in app

--- brain/urls.py
+++ brain/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for brain project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+
+    
